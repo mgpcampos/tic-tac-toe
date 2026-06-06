700d59bb1d12bcc975d67b8f6742fab1addd00e1
mgpc <109235896+mgpcampos@users.noreply.github.com>
2026-06-06 12:24:05 -0300

    Implement printBoard function to display the tic-tac-toe board

--- main.c
+++ main.c
@@ -6,6 +6,15 @@ char board[3][3] = {
     {' ', ' ', ' '}
 };
 
+void printBoard(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
 
 }
