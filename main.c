a4205607930d7fee726c2e371a7cdcf05e276361
mgpc <109235896+mgpcampos@users.noreply.github.com>
2026-06-06 12:25:02 -0300

    Add makeMove function to handle player moves in the game

--- main.c
+++ main.c
@@ -15,6 +15,22 @@ void printBoard(char board[3][3]) {
     }
 }
 
+int makeMove(char board[3][3], char symbol) {
+    int row, col;
+    printf("Enter row (0-2): ");
+    scanf("%d", &row);
+    printf("Enter column (0-2): ");
+    scanf("%d", &col);
+
+    if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+        board[row][col] = symbol;
+        return 1; // Move successful
+    } else {
+        printf("Invalid move. Try again.\n");
+        return 0; // Move failed
+    }
+}
+
 int main() {
 
 }
