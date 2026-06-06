58508ad057397457035f20d62413e86ee2e89e64
mgpc <109235896+mgpcampos@users.noreply.github.com>
2026-06-06 12:57:44 -0300

    Translate comments

--- main.c
+++ main.c
@@ -17,18 +17,41 @@ void printBoard(char board[3][3]) {
 
 int makeMove(char board[3][3], char symbol) {
     int row, col;
-    printf("Enter row (0-2): ");
+    printf("Insira a linha (0-2): ");
     scanf("%d", &row);
-    printf("Enter column (0-2): ");
+    printf("Insira a coluna (0-2): ");
     scanf("%d", &col);
 
     if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
         board[row][col] = symbol;
-        return 1; // Move successful
+        return 1; // Jogada válida
     } else {
-        printf("Invalid move. Try again.\n");
-        return 0; // Move failed
+        printf("Jogada inválida. Tente novamente.\n");
+        return 0; // Jogada inválida
     }
+}
+
+char checkWinner(char board[3][3]) {
+    // Verifica as linhas
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+            return board[i][0];
+        }
+    }
+    // Verifica as colunas
+    for (int j = 0; j < 3; j++) {
+        if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
+            return board[0][j];
+        }
+    }
+    // Verifica as diagonais
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        return board[0][0];
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+        return board[0][2];
+    }
+    return ' '; // Nenhum vencedor ainda
 }
 
 int main() {
