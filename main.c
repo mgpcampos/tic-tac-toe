3636057e70362b1f933a30311ea662520099a6de
mgpc <109235896+mgpcampos@users.noreply.github.com>
2026-06-06 22:59:25 -0300

    Add limpar_tela function to clear console based on OS

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TAMANHO 3
 #define CELULA_VAZIA ' '
+
+void limpar_tela() {
+#ifdef _WIN32
+    // Windows
+    system("cls");
+#else
+    // macOS e Linux
+    system("clear");
+#endif
+    fflush(stdout);
+}
 
 void inicializar_tabuleiro(char tabuleiro[3][3]) {
     for (int i = 0; i < TAMANHO; i++) {
